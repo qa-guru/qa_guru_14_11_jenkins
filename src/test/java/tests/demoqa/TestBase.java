88d5497522b67e84dfd0e8a1697fc4779c918d4d
svasenkov <aanher@gmail.com>
2022-08-30 21:35:16 +0300

    added demoqa test

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -2,7 +2,9 @@ package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
@@ -16,4 +18,10 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+    }
 }
